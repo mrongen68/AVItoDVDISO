4b0fbbf5a1c34bbd51ebfa578130f3eb22d5ad19
mrongen68 <151833542+mrongen68@users.noreply.github.com>
2026-01-05 14:43:47 +0100

    Remove unnecessary comments from App.xaml.cs

--- src/AVItoDVDISO.App/App.xaml.cs
+++ src/AVItoDVDISO.App/App.xaml.cs
@@ -1,8 +1,6 @@
-# ---------------------------------
-# File: src\AVItoDVDISO.App\App.xaml.cs
-# ---------------------------------
 namespace AVItoDVDISO.App;
 
 public partial class App : System.Windows.Application
 {
+
 }
